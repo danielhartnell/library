34d44f0473e849b219a483f0d44e240a00983d6d
Daniel Hartnell <dhartnell@newrelic.com>
2015-10-07 16:22:44 -0700

    ran db:create:all db:migrate and updated index content.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004061336) do
+ActiveRecord::Schema.define(version: 20151003054905) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -24,13 +27,6 @@ ActiveRecord::Schema.define(version: 20151004061336) do
     t.string   "nerd"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string   "name"
-    t.string   "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
